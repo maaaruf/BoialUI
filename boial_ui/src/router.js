b121a7abb4b9feb44f345b1dc777e7290dd215f6
maaaruf <marufbdonline@gmail.com>
2021-09-01 16:05:13 +0600

    Added Not Found

--- boial_ui/src/router.js
+++ boial_ui/src/router.js
@@ -1,7 +1,10 @@
+import NotFound from "./shared/components/404/404";
+import { Route } from "react-router";
+
 export default function Router() {
     return (
         <Route exact path="/404">
-            
+            <NotFound />
         </Route>
     )
 }
