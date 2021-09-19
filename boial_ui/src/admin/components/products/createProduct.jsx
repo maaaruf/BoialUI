0808ee17ff37d3fc9e84445f19f31868ca1ba1a5
maaaruf <marufbdonline@gmail.com>
2021-09-20 01:11:14 +0600

    Added dropdown of categories

--- boial_ui/src/admin/components/products/createProduct.jsx
+++ boial_ui/src/admin/components/products/createProduct.jsx
@@ -29,9 +29,9 @@ const useStyles = makeStyles((theme) => ({
 export default function CreateProduct() {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const cat = useSelector((store) => store.categoryStore);
-  console.log(cat, "category from selector");
-  
+  const categories = useSelector((store) => store.CategoryStore.data);
+  console.log(categories, "category from selector");
+
   const [product, setProduct] = useState({
     title: "",
     price: 0,
@@ -43,78 +43,71 @@ export default function CreateProduct() {
     }
   });
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(loadCategoriesAction());
-  },[]);
+  }, []);
 
   const updateProduct = (property, value) => {
-
     setProduct(product => ({ ...product, [property]: value }));
-    console.log(product);
+  }
+
+  const updateProductCategory = (property, value) => {
+    setProduct(product => ({ ...product, category: { [property]: value } }));
+
+    console.log(product, "======================pricd");
   }
 
   const createProductHandler = (e) => {
     e.preventDefault();
     product.price = parseInt(product.price);
     product.stock = parseInt(product.stock);
-    console.log(product, "product from Create product page ========");
-
-    console.log(cat, "category from selector");
     dispatch(createProductAction(product));
   }
 
   const handleImage = (e) => {
     setProduct({ ...product, image: e.base64 });
-    toastNotify("Product Image is ready", SUCCESSFUL);
+    toastNotify("Image is ready to upload", SUCCESSFUL);
   };
 
-  
-
   return (
-    <Grid container spacing={3} justifyContent="center">
-      <Grid
-        item
-        lg={6}>
+    <>
+      <p>Create Category</p>
 
-        <form className={classes.root} noValidate autoComplete="off">
+      <br />
+      <form className={classes.root} noValidate autoComplete="off">
 
-          <TextField id="outlined-basic" label="Title" variant="outlined" fullWidth onChange={e => updateProduct("title", e.target.value)} />
+        <TextField id="outlined-basic" label="Title" variant="outlined" fullWidth onChange={e => updateProduct("title", e.target.value)} />
 
-          <TextField id="outlined-basic" label="description" variant="outlined" fullWidth onChange={e => updateProduct("description", e.target.value)} />
+        <TextField id="outlined-basic" label="description" variant="outlined" fullWidth onChange={e => updateProduct("description", e.target.value)} />
+        <div>
+          <FileBase64 onDone={handleImage} multiple={false} />
+        </div>
+        <TextField id="outlined-basic" label="stock" variant="outlined" onChange={e => updateProduct("stock", e.target.value)} />
+        <TextField id="outlined-basic" label="price" variant="outlined" onChange={e => updateProduct("price", e.target.value)} />
+
+        <FormControl variant="filled" className={classes.formControl}>
+          <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            //value={categories[0]._id}
+            onChange={(e) => updateProductCategory("_id", e.target.value)}
+          >
+
+
+            {
+              categories?.map((row) => (<MenuItem value={row._id}>{row.name}</MenuItem>))
+            }
+          </Select>
+
           <div>
-            <FileBase64 onDone={handleImage} multiple={false} />
+            <button class="button" onClick={(e) => createProductHandler(e)}>
+              <span>Create Product</span>
+            </button>
           </div>
-          <TextField id="outlined-basic" label="stock" variant="outlined" onChange={e => updateProduct("stock", e.target.value)} />
-          <TextField id="outlined-basic" label="price" variant="outlined" onChange={e => updateProduct("price", e.target.value)} />
 
-          <FormControl variant="filled" className={classes.formControl}>
-            <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
-            <Select
-              labelId="demo-simple-select-filled-label"
-              id="demo-simple-select-filled"
-            //value={10}
-            // onChange={handleChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-
-            <div>
-              <button class="button" onClick={(e) => createProductHandler(e)}>
-                <span>Create Product</span>
-              </button>
-            </div>
-
-          </FormControl>
-        </form>
-      </Grid>
-    </Grid>
-
-
-
+        </FormControl>
+      </form>
+    </>
   );
 }
