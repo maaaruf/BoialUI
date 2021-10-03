b8485675846e1b3fe3ab27b1f1972f36bef16e57
maaaruf <marufbdonline@gmail.com>
2021-10-03 16:07:27 +0600

    Added pagination

--- boial_ui/src/shared/components/products/products.jsx
+++ boial_ui/src/shared/components/products/products.jsx
@@ -4,10 +4,27 @@ import { useSelector } from "react-redux";
 import { loadProductsAction } from "../../../store/action/productAction";
 import Product from "../product/product";
 import classes from './products.css';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import * as React from 'react';
 
 export default function Products() {
-    const products = useSelector((store) => store.ProductsStore.data);
     const dispatch = useDispatch();
+    const products = useSelector((store) => store.ProductsStore.data);
+    const [page, setPage] = React.useState(1);
+    // const [productsToDisplay, setProductsToDisplay] = useState(0);
+
+    const postPerPage = 4;
+    const indexOfLastPost = page * postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const productsToDisplay = products.slice(indexOfFirstPost, indexOfLastPost);
+    const pageCount = Math.ceil(products.length / postPerPage);
+
+
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
 
     useState(() => {
         dispatch(loadProductsAction());
@@ -17,11 +34,15 @@ export default function Products() {
         <>
             <div className="videos">
                 {
-                    products.map((product) => (
-                        <Product product = {product} />
+                    productsToDisplay.map((product) => (
+                        <Product product={product} />
                     ))
                 }
             </div>
+
+            <Stack spacing={2}>
+                <Pagination count={pageCount} page={page} onChange={handleChange} />
+            </Stack>
         </>
     );
 }
